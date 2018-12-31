fb6633ebec4f7d16a98c7121aded5ed49523321b
reario <reario@inwind.it>
2018-12-31 18:44:42 +0100

    aggiunti i flag -f -n. -f se viene lanciato con -f[r,s] se lo trova spento allora non commuta. se viene lanciato con -n e lo trova acceso allora non commuta

--- faretti.c
+++ faretti.c
@@ -45,25 +45,27 @@ int operate(modbus_t *mb,
       return -1;
     }
   if (what_in >=0) {
-  printf("INGRESSI PRIMA erano %s \n",read_single_state(in[num_reg_in],what_in)?"ON":"OFF");
-}
-  if ( interruttore(mb,out,num_reg_out,what_out) == -1 ) {
-  printf("Errore WRITE (IN or OUT) registro %d OTB: %i\n",addr_reg_out,errno);
-  return -1;
-}
+    printf("INGRESSI PRIMA erano %s \n",read_single_state(in[num_reg_in],what_in)?"ON":"OFF");
+  }
+  /*********************************************************/
+      if ( interruttore(mb,out,num_reg_out,what_out) == -1 ) {
+	printf("Errore WRITE (IN or OUT) registro %d OTB: %i\n",addr_reg_out,errno);
+	return -1;
+      }
+  /*********************************************************/
   // serve per far assestare gli ingressi al fine di leggeere un valore di questi congruente con quanto 
   // si è appena scritto
   // se non mettessimo lo sleep ci potremmo trovare che gli ingressi sono ancora allo stato precedente 
   sleep(1);
   if (modbus_read_registers(mb, addr_reg_in, num_reg_in_to_read, in) == -1 || 
-    modbus_read_registers(mb, addr_reg_out, num_reg_out_to_write, out) == -1)
+      modbus_read_registers(mb, addr_reg_out, num_reg_out_to_write, out) == -1)
     {
-  printf("Errore READ (IN or OUT) registro %i or %i OTB: %i\n",addr_reg_in,addr_reg_out,errno);
-  return -1;
-}
-   if (what_in >=0) {
-     printf("INGRESSI DOPO sono   %s \n",read_single_state(in[addr_reg_in],what_in)?"ON":"OFF");
-   }
+      printf("Errore READ (IN or OUT) registro %i or %i OTB: %i\n",addr_reg_in,addr_reg_out,errno);
+      return -1;
+    }
+  if (what_in >=0) {
+    printf("INGRESSI DOPO sono   %s \n",read_single_state(in[addr_reg_in],what_in)?"ON":"OFF");
+  }
   return 0;
 }
   
@@ -76,7 +78,7 @@ int main (int argc, char ** argv) {
   uint16_t otb_out[10];
   
   int ch;
-  int rflag,sflag,aflag;
+  int rflag=0,sflag=0,aflag=0,nflag=0,fflag=0;
   // char *rstate,*sstate;
 
   mb_otb = modbus_new_tcp("192.168.1.11",PORT);
@@ -88,7 +90,7 @@ int main (int argc, char ** argv) {
       exit(1);
     } 
 
-  while ( (ch = getopt (argc, argv, "ars")) != -1) {
+  while ( (ch = getopt (argc, argv, "afnrs")) != -1) {
     switch (ch) {
     case 'r':
       rflag=1;
@@ -101,6 +103,14 @@ int main (int argc, char ** argv) {
       //---------------------------------------------
       aflag=1;
       break;
+    case 'f':
+      //---------------------------------------------
+      fflag=1;
+      break;
+    case 'n':
+      //---------------------------------------------
+      nflag=1;
+      break;
     default:
       printf("--------\n");
       abort();
@@ -108,23 +118,150 @@ int main (int argc, char ** argv) {
   }
   /*****************************SOPRA*************************************/
   if (rflag == 1) {
-    if (operate(mb_otb, // mb
-		otb_in, // in array di registri INGRESSI (Analogici, Digitali)
-		otb_out, // out array di registri USCITE (Analogiche, Digitali)
-		FARI_ESTERNI_IN_SOPRA, // what_in numero bit all'interno del registro INGRESSI (se <0 non check gli ingressi)
-		FARI_ESTERNI_SOPRA, // what_out numero del bit all'interno del registro USCITE (bit da modificare)
-		0, // addr_reg_in registo partenza degli INGRESSO sul device
-		100, // addr_reg_out registo partenza delle USCITE sul device
-		3, // num_reg_in_to_read numero registri in INGRESSO da leggere
-		3, // num_reg_in_to_write numero registri in USCITA da leggere
-		0, // numero del registro in INGRESSO interessato alla operazione
-		0 // numero del registro in USCITA interessato alla operazione
-    
-    ) != 0 ) {
-      exit(-1);
-    } 
-  }
+    modbus_read_registers(mb_otb, 0, 1, otb_in);    
+    if (fflag == 1) {
+      if ( read_single_state(otb_in[0],FARI_ESTERNI_IN_SOPRA) ) { 
+      /* è acceso ed ho il flag f quindi lo spengo commutando */
+	if (operate(mb_otb, // mb
+		    otb_in, // in array di registri INGRESSI (Analogici, Digitali)
+		    otb_out, // out array di registri USCITE (Analogiche, Digitali)
+		    FARI_ESTERNI_IN_SOPRA, //what_in numero bit nel registro INGRESSI(se<0 non check gli ingressi)
+		    FARI_ESTERNI_SOPRA, // what_out numero del bit all'interno del registro USCITE (bit da modificare)
+		    0, // addr_reg_in registo partenza degli INGRESSO sul device
+		    100, // addr_reg_out registo partenza delle USCITE sul device
+		    3, // num_reg_in_to_read numero registri in INGRESSO da leggere
+		    3, // num_reg_in_to_write numero registri in USCITA da leggere
+		    0, // numero del registro in INGRESSO interessato alla operazione
+		    0 // numero del registro in USCITA interessato alla operazione
+		    
+	) != 0 ) {
+	  exit(-1);
+	} // operate
+	
+      } else {
+	printf("Il faretto SOPRA era già spento e lo lascio spento perchè ho il flag -f\n");
+      }
+    } // fflag == 1
+  
+    if (nflag == 1) {
+      if ( (! read_single_state(otb_in[0],FARI_ESTERNI_IN_SOPRA)) ) { 
+	/* è spento ed ho il flag n quindi lo accendo commutando */
+	if (operate(mb_otb, // mb
+		    otb_in,  // in array di registri INGRESSI (Analogici, Digitali)
+		    otb_out, // out array di registri USCITE (Analogiche, Digitali)
+		    FARI_ESTERNI_IN_SOPRA, //what_in numero bit nel registro INGRESSI(se < 0 non check gli ingressi)
+		    FARI_ESTERNI_SOPRA, // what_out numero del bit all'interno del registro USCITE (bit da modificare)
+		    0, // addr_reg_in registo partenza degli INGRESSO sul device
+		    100, // addr_reg_out registo partenza delle USCITE sul device
+		    3, // num_reg_in_to_read numero registri in INGRESSO da leggere
+		    3, // num_reg_in_to_write numero registri in USCITA da leggere
+		    0, // numero del registro in INGRESSO interessato alla operazione
+		    0 // numero del registro in USCITA interessato alla operazione
+		    
+	) != 0 ) {
+	  exit(-1);
+	} // operate
+      } else {
+	printf("Il faretto SOPRA era già acceso e lo lascio acceso perchè ho il flag -n\n");
+      }
+    } // nflag == 1
+  
+    if ( (nflag != 1) && (fflag != 1) ) {
+      /*non ho ne n ne f flag e quindi commuto*/
+	if (operate(mb_otb, // mb
+		    otb_in, // in array di registri INGRESSI (Analogici, Digitali)
+		    otb_out, // out array di registri USCITE (Analogiche, Digitali)
+		    FARI_ESTERNI_IN_SOPRA, //what_in numero bit nel registro INGRESSI(se<0 non check gli ingressi)
+		    FARI_ESTERNI_SOPRA, // what_out numero del bit all'interno del registro USCITE (bit da modificare)
+		    0, // addr_reg_in registo partenza degli INGRESSO sul device
+		    100, // addr_reg_out registo partenza delle USCITE sul device
+		    3, // num_reg_in_to_read numero registri in INGRESSO da leggere
+		    3, // num_reg_in_to_write numero registri in USCITA da leggere
+		    0, // numero del registro in INGRESSO interessato alla operazione
+		    0 // numero del registro in USCITA interessato alla operazione
+		    
+	) != 0 ) {
+	  exit(-1);
+	} // operate      
+    }
+
+  } // rflag == 1 
+
   /******************************************************************/
+
+  ////////////////////////////////////////////////////
+  if (sflag == 1) {
+    modbus_read_registers(mb_otb, 0, 1, otb_in);    
+    if (fflag == 1) {
+      if ( read_single_state(otb_in[0],FARI_ESTERNI_IN_SOTTO) ) { 
+      /* è acceso ed ho il flag f quindi lo spengo commutando */
+	if (operate(mb_otb, // mb
+		    otb_in, // in array di registri INGRESSI (Analogici, Digitali)
+		    otb_out, // out array di registri USCITE (Analogiche, Digitali)
+		    FARI_ESTERNI_IN_SOTTO, //what_in numero bit nel registro INGRESSI(se<0 non check gli ingressi)
+		    FARI_ESTERNI_SOTTO, // what_out numero del bit all'interno del registro USCITE (bit da modificare)
+		    0, // addr_reg_in registo partenza degli INGRESSO sul device
+		    100, // addr_reg_out registo partenza delle USCITE sul device
+		    3, // num_reg_in_to_read numero registri in INGRESSO da leggere
+		    3, // num_reg_in_to_write numero registri in USCITA da leggere
+		    0, // numero del registro in INGRESSO interessato alla operazione
+		    0 // numero del registro in USCITA interessato alla operazione
+		    
+	) != 0 ) {
+	  exit(-1);
+	} // operate
+	
+      } else {
+	printf("Il faretto SOTTO era già spento e lo lascio spento perchè ho il flag -f\n");
+      }
+    } // fflag == 1
+  
+    if (nflag == 1) {
+      if ( (! read_single_state(otb_in[0],FARI_ESTERNI_IN_SOTTO)) ) {
+      /* è spento ed ho il flag n quindi lo accendo commutando */
+	if (operate(mb_otb, // mb
+		    otb_in, // in array di registri INGRESSI (Analogici, Digitali)
+		    otb_out, // out array di registri USCITE (Analogiche, Digitali)
+		    FARI_ESTERNI_IN_SOTTO, //what_in numero bit nel registro INGRESSI(se<0 non check gli ingressi)
+		    FARI_ESTERNI_SOTTO, // what_out numero del bit all'interno del registro USCITE (bit da modificare)
+		    0, // addr_reg_in registo partenza degli INGRESSO sul device
+		    100, // addr_reg_out registo partenza delle USCITE sul device
+		    3, // num_reg_in_to_read numero registri in INGRESSO da leggere
+		    3, // num_reg_in_to_write numero registri in USCITA da leggere
+		    0, // numero del registro in INGRESSO interessato alla operazione
+		    0 // numero del registro in USCITA interessato alla operazione
+		    
+	) != 0 ) {
+	  exit(-1);
+	} // operate
+      } else {
+	printf("Il faretto SOTTO era già acceso e lo lascio acceso perchè ho il flag -n\n");
+      }
+    } // nflag == 1
+  
+    if ( (nflag != 1) && (fflag != 1) ) {
+      /*non ho ne n ne f flag e quindi commuto*/
+	if (operate(mb_otb, // mb
+		    otb_in, // in array di registri INGRESSI (Analogici, Digitali)
+		    otb_out, // out array di registri USCITE (Analogiche, Digitali)
+		    FARI_ESTERNI_IN_SOTTO, //what_in numero bit nel registro INGRESSI(se<0 non check gli ingressi)
+		    FARI_ESTERNI_SOTTO, // what_out numero del bit all'interno del registro USCITE (bit da modificare)
+		    0, // addr_reg_in registo partenza degli INGRESSO sul device
+		    100, // addr_reg_out registo partenza delle USCITE sul device
+		    3, // num_reg_in_to_read numero registri in INGRESSO da leggere
+		    3, // num_reg_in_to_write numero registri in USCITA da leggere
+		    0, // numero del registro in INGRESSO interessato alla operazione
+		    0 // numero del registro in USCITA interessato alla operazione
+		    
+	) != 0 ) {
+	  exit(-1);
+	} // operate      
+    } //nflag !=1 && ...
+
+  } // sflag == 1 
+
+  ////////////////////////////////////////////////////
+  /*
   if (sflag == 1) {
     if (operate(mb_otb, // modbus context
 		otb_in, // in array di registri INGRESSI (Analogici, Digitali)
@@ -141,6 +278,7 @@ int main (int argc, char ** argv) {
       exit(-1);
     } 
   } 
+  */
   /******************************************************************/
   if (aflag == 1) {
     printf("--- aflag ---\n");
